5039304b680690024552d878d1ebe5dc0e7a54c7
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-05-30 13:04:50 -0300

    refactoring category templates

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger.json";
+ import swaggerFile from "./swagger.json";
+
+import "./database"
 
 const app = express();
 
